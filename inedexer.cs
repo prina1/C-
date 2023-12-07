fe6c436cecaf60b9e4b591ccad86469a75239d9c
Prina Tamang <121554323+prina1@users.noreply.github.com>
2023-12-07 07:30:32 +0545

    commit

---
+++ inedexer.cs
@@ -0,0 +1,23 @@
+// public class TempRecord 
+// {
+
+//     //Array of temperatures values 
+//     float[] temps = new float[10] { 56.2F, 56.7F, 56.5F, 56.9F, 58.8F, 61.3F, 65.9F, 62.1F, 59.2F, 57.5F };
+
+//     //enable client code to validate input 
+//     //when enable your indexer 
+//     public float this[int index]
+//     {
+//         get => temps[index];
+//         set => temps[index] = value;
+
+
+//     }
+
+
+
+// }
+// class program
+// {
+//     static void Manin
+// }
---
+++ inheritance.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Inheritance{
+    internal class Program{
+        public class Animal{
+            public void Eat(){
+                Console.WriteLine("Animal is eating");
+            }
+            public class Dog: Animal{
+                public void Bark(){
+                    Console.WriteLine("Dog is barking");
+                }
+            }
+        static void Main(string[] args)
+        {Dog hero=new Dog();
+            hero.Eat();
+            hero.Bark();
+           
+           }
+        }
+    }
+}
