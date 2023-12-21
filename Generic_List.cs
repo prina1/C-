207538d91a6e4ac4eb9fdd62fe65eca23138553c
Prina Tamang <121554323+prina1@users.noreply.github.com>
2023-12-21 07:48:09 +0545

    add

---
+++ Generic_List.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+class Program{
+    static void Main()
+    {
+        //creating a generic list of type T
+        List<int>integerList = new List<int>();
+        List<string>stringList = new List<string>();
+        List<double>doubleList = new List<double>();
+
+        //adding elements to the list
+        integerList.Add(1);
+        integerList.Add(2);
+        integerList.Add(3);
+
+        stringList.Add("Apple");
+        stringList.Add("Orrange");
+        stringList.Add("Banana");
+
+        doubleList.Add(3.4);
+        doubleList.Add(5.6);
+        doubleList.Add(7.8);
+
+        Console.WriteLine("Integer List");
+        DisplayList(integerList);
+        integerList.Sort();
+        integerList.Remove(2);
+        integerList.RemoveAt(0);
+
+        DisplayList(integerList);
+
+
+        Console.WriteLine("\n sring List");
+        DisplayList(stringList);
+
+        Console.WriteLine("\n Double List");
+        DisplayList(doubleList);
+
+    }
+    //helper methid to display the content of generic list
+    static void DisplayList<T>(List<T>List)
+    {
+        foreach(T item in List)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+
+    }
+}
--- dictonary.cs
+++ dictonary.cs
@@ -1,23 +1,28 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 
-class Program 
-{
-    static void Main()
-    {
-        //creating a dictonary with string keys and int yvalues 
-        Dictionary<string,int> ages = new Dictionary<string,int>();
+// class Program 
+// {
+//     static void Main()
+//     {
+//         //creating a dictonary with string keys and int yvalues 
+//         Dictionary<string,int> ages = new Dictionary<string,int>();
 
-        //adding key value pair to the dictonary
-        ages["Alice"]= 25;
-        ages["Bob"]= 30;
-        ages["Sudha"]=22;
+//         //adding key value pair to the dictonary
+//         ages["Alice"]= 25;
+//         ages["Bob"]= 30;
+//         ages["Sudha"]=22;
 
-        //acissing value by key
-        Console.WriteLine(ages["Sudha"]);
+//         //acissing value by key
+//         Console.WriteLine(ages["Sudha"]);
+
+//         foreach (var pair in ages)
+//         {
+//             Console.WriteLine($"{pair.Key}{pair.Value }");
+//         }
 
 
     
 
-    }
-}
+//     }
+// }
